66b62261c226257451c4a1eacd6d012425d1a13f
RLittlesII <6969701+RLittlesII@users.noreply.github.com>
2019-11-07 02:23:14 -0600

    Added PopupRoutingState

---
+++ src/ReactiveUI.Plugins.Popup/PopupRoutingState.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using ReactiveUI;
+using Rg.Plugins.Popup.Contracts;
+using Rg.Plugins.Popup.Services;
+
+namespace RxUI.Plugins.Popup
+{
+    public class PopupRoutingState : RoutingState
+    {
+        private IPopupNavigation _popupNavigation;
+
+        [DataMember]
+        private readonly ObservableCollection<IRoutableViewModel> _popupNavigationStack;
+
+        public PopupRoutingState()
+        {
+            _popupNavigation = PopupNavigation.Instance;
+
+            var navigateScheduler = RxApp.MainThreadScheduler; // TODO: Allow user to set scheduler.
+
+            var countAsBehavior = Observable.Concat(
+                Observable.Defer(() => Observable.Return(NavigationStack.Count)),
+                NavigationChanged.CountChanged().Select(_ => NavigationStack.Count));
+
+            NavigatePopup = ReactiveCommand.CreateFromObservable<IRoutableViewModel, IRoutableViewModel>(
+                vm =>
+                {
+                    if (vm == null)
+                    {
+                        throw new Exception($"Navigate must be called on an {nameof(IRoutableViewModel)}");
+                    }
+
+                    _popupNavigationStack.Add(vm);
+                    return Observable.Return(vm);
+                },
+                outputScheduler: navigateScheduler);
+
+            NavigatePopupBack = ReactiveCommand.CreateFromObservable<Unit, Unit>(
+                () =>
+                {
+                    _popupNavigationStack.RemoveAt(NavigationStack.Count - 1);
+                    return Observable.Return(Unit.Default);
+                },
+                countAsBehavior.Select(x => x > 1),
+                navigateScheduler);
+        }
+
+        /// <summary>
+        /// Gets or sets a command that navigates to the a new element in the stack - the Execute parameter
+        /// must be a ViewModel that implements IRoutableViewModel.
+        /// </summary>
+        [IgnoreDataMember]
+        public ReactiveCommand<IRoutableViewModel, IRoutableViewModel> NavigatePopup { get; protected set; }
+
+        /// <summary>
+        /// Gets or sets a command which will navigate back to the previous element in the stack.
+        /// </summary>
+        [IgnoreDataMember]
+        public ReactiveCommand<Unit, Unit> NavigatePopupBack { get; protected set; }
+
+    }
+}
